953feb95610627ec46833967a9ac157e46ce0eec
sianjyun <sianjyun@usc.edu>
2023-12-14 14:58:05 -0800

    initialize

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MantineProvider } from '@mantine/core';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <MantineProvider
+    theme={{
+      // Your theme override here
+      colorScheme: 'light', // or 'dark'
+    }}
+    withGlobalStyles
+    withNormalizeCSS
+  >
     <App />
-  </React.StrictMode>
+  </MantineProvider>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
